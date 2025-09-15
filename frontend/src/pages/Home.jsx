cafede012e3053a5cdfffa9541adcde487580eed
Yuhua Lin <yuhual2@student.unimelb.edu.au>
2025-09-15 13:57:57 +1000

    Updated Mobile Rendering v0.1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -197,8 +197,10 @@ function Home() {
         style={{textAlign: "center"}} 
         onClick={() => navigate("/game")}
       >
-        <h1 className="title1">PowerUp</h1>
-        <h1 className="title2">O O S D</h1>
+        <div className="title-wrap">
+          <h1 className="title1">PowerUp</h1>
+          <h1 className="title2">O O S D</h1>
+        </div>
 
         <img
           src={
